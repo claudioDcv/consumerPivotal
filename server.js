bdee56c402c480f8bb4b4e3651f5545532ed5fe3
Jesús Urrutia <jesus.urrutia@gmail.com>
2017-05-31 19:00:35 -0400

    Modify to make request to Pivotal API with request library

--- server.js
+++ server.js
@@ -1,31 +1,58 @@
 const HTTP = require('http');
 const EXEC_PROCESS = require("./exec_process.js");
 const createMark = require('./createMark.js');
+const rp = require('request-promise');
+
+var PORT = process.env.PORT || 8899;
+var TOKEN = process.env.PIVOTAL_TOKEN;
 
 HTTP.createServer(function(req, res) {
 
-    var url = require('url');
-    var url_parts = url.parse(req.url, true);
-    var query = url_parts.query;
+  var url = require('url');
+  var url_parts = url.parse(req.url, true);
+  var query = url_parts.query;
 
-    var QUERYLABEL = query.label ? query.label : '';
-    var PROJECT_ID = query.project_id ? query.project_id : '';
+  var QUERYLABEL = query.label ? query.label : '';
+  var PROJECT_ID = query.project_id ? query.project_id : '';
 
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
 
 	console.log("Params: label:" + QUERYLABEL + ' project_id:' + PROJECT_ID);
 
-    EXEC_PROCESS.result("sh consumer.sh " + QUERYLABEL + ' ' + PROJECT_ID, function(err, response) {
-        EXEC_PROCESS.result("sh consumerEpics.sh " + PROJECT_ID, function(err2, response2) {
-            if (!err ||  !err2) {
+  var stories_options = {
+    uri: 'https://www.pivotaltracker.com/services/v5/projects/' + PROJECT_ID + '/stories',
+    qs: {
+      with_label: QUERYLABEL // -> uri + '?access_token=xxxxx%20xxxxx'
+    },
+    headers: {
+      'X-TrackerToken': TOKEN
+    },
+    json: false // Automatically parses the JSON string in the response
+  };
+  var epics_options = {
+    uri: 'https://www.pivotaltracker.com/services/v5/projects/' + PROJECT_ID + '/epics',
+    headers: {
+      'X-TrackerToken': TOKEN
+    },
+    json: false // Automatically parses the JSON string in the response
+  };
 
-                res.end(createMark.createMark(response, response2, QUERYLABEL));
-            } else {
-                res.end("Error: ", err || err2);
-            }
-        });
-    });
-}).listen(8899);
-console.log('Server listening on port 8899');
+  var stories_rq = rp(stories_options);
+  var epics_rq = rp(epics_options);
+
+  Promise.all([stories_rq, epics_rq])
+    .then(function (response) {
+      var stories = response[0];
+      var epics = response[1];
+
+      res.end(createMark.createMark(stories, epics, QUERYLABEL));
+    })
+    .catch(function (e) {
+      res.end("Error: ", e);
+    })
+  ;
+}).listen(PORT, function () {
+  console.log('Listen on PORT: ' + PORT);
+});
