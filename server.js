782680da4e45bce5db04276381e4144394a57e62
Jesús Urrutia <jesus.urrutia@gmail.com>
2017-06-16 20:24:07 -0400

    Modify to use express

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
-const HTTP = require('http');
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
 const rp = require('request-promise');
-const URL = require('url');
 const createMark = require('./createMark.js');
-const CONSUMER = require("./consumer.js");
+const Pivotal = require('./pivotal-api-client');
+
 
 const PORT = process.env.PORT || 8899;
 const TOKEN = process.env.PIVOTAL_TOKEN;
 
-HTTP.createServer(function(req, res) {
+const pivotal = new Pivotal(TOKEN);
 
-  var explainError = '';
-  var url_parts = URL.parse(req.url, true);
-  var query = url_parts.query;
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: app
+});
 
-  var QUERYLABEL = query.label ? query.label : '';
-  var PROJECT_ID = query.project_id ? query.project_id : '';
+app.get('/', function (req, res) {
+  res.render('index.html');
+});
 
-// Formater Error Out Explain
-  if (!QUERYLABEL)
-    explainError += 'label no puede estar vacio | ';
-  if (!PROJECT_ID)
-    explainError += 'project_id no puede estar vacio | ';
+app.get('/reports/daily', function (req, res) {
+  projects = pivotal.getProjects();
+  labels = pivotal.getLabels('1948407');
 
-  console.log("Params: label:" + QUERYLABEL + ' project_id:' + PROJECT_ID);
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-
-  var stories_rq = rp(CONSUMER.consumer(QUERYLABEL, TOKEN, PROJECT_ID).stories_options);
-  var epics_rq = rp(CONSUMER.consumer(QUERYLABEL, TOKEN, PROJECT_ID).epics_options);
-
-  Promise.all([stories_rq, epics_rq])
+  Promise.all([projects. labels])
     .then(function (response) {
-      var stories = response[0];
-      var epics = response[1];
+      res.render('reports-daily.html', {
+        projects: response[0],
+        labels: response[1],
+      });
+    });
+});
 
-      res.end(createMark.createMark(stories, epics, QUERYLABEL));
-    })
-    .catch(function (e) {
-      res.end(`Error: ${explainError}`, e);
-    })
-  ;
-}).listen(PORT, function () {
-  console.log('Listen on PORT: ' + PORT);
+app.get('/reports/weekly', function (req, res) {
+  res.render('reports-weekly.html');
+});
+
+app.listen(PORT, function () {
+  console.log('Listen on: http://0.0.0.0:' + PORT);
 });
